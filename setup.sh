1c2860353815d71c445ba6060e4f528535dfef10
Ubuntu <ubuntu@ip-172-31-36-160.ap-south-1.compute.internal>
2024-07-02 10:42:33 +0000

    work

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ ip route add default via 127.0.0.1 dev lo
 route -n
 
 # iptables rules to route traffic to transparent proxy
+update-alternatives --set iptables /usr/sbin/iptables-legacy
 iptables -A OUTPUT -t nat -p tcp --dport 1:65535 ! -d 127.0.0.1  -j DNAT --to-destination 127.0.0.1:1200
 iptables -t nat -A POSTROUTING -o lo -s 0.0.0.0 -j SNAT --to-source 127.0.0.1
 iptables -L -t nat
